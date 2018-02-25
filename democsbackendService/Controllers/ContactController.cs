9d5cfa4877748111b57eb9810bb99ed65431779d
Martín Basurto Díaz <mbdz.msi@hotmail.com>
2018-02-25 10:08:06 -0600

    Add project files.

---
+++ democsbackendService/Controllers/ContactController.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.OData;
+using Microsoft.Azure.Mobile.Server;
+using democsbackendService.DataObjects;
+using democsbackendService.Models;
+
+namespace democsbackendService.Controllers
+{
+    public class ContactController : TableController<Contact>
+    {
+        protected override void Initialize(HttpControllerContext controllerContext)
+        {
+            base.Initialize(controllerContext);
+            democsbackendContext context = new democsbackendContext();
+            DomainManager = new EntityDomainManager<Contact>(context, Request);
+        }
+
+        // GET tables/TodoItem
+        public IQueryable<Contact> GetAllTodoItems()
+        {
+            return Query();
+        }
+
+        // GET tables/TodoItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public SingleResult<Contact> GetTodoItem(string id)
+        {
+            return Lookup(id);
+        }
+
+        // PATCH tables/TodoItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public Task<Contact> PatchTodoItem(string id, Delta<Contact> patch)
+        {
+            return UpdateAsync(id, patch);
+        }
+
+        // POST tables/TodoItem
+        public async Task<IHttpActionResult> PostTodoItem(Contact item)
+        {
+            Contact current = await InsertAsync(item);
+            return CreatedAtRoute("Tables", new { id = current.Id }, current);
+        }
+
+        // DELETE tables/TodoItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public Task DeleteTodoItem(string id)
+        {
+            return DeleteAsync(id);
+        }
+    }
+}
